36e0cf2e588489005fb7d69a076f5d553593e411
azizulhoq953 <123456+azizulhoq2041@gmail.com>
2025-11-22 15:06:27 +0600

    auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,35 +5,6 @@ import config from '../../config';
 import { jwtHelper } from '../../helpers/jwtHelper';
 import ApiError from '../../errors/ApiError';
 
-// const auth = (...roles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//         const tokenWithBearer = req.headers.authorization;
-//         if (!tokenWithBearer) {
-//             throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
-//         }
-  
-//         if (tokenWithBearer && tokenWithBearer.startsWith('Bearer')) {
-//             const token = tokenWithBearer.split(' ')[1];
-  
-//             //verify token
-//             const verifyUser = jwtHelper.verifyToken(
-//                 token,
-//                 config.jwt.jwt_secret as Secret
-//             );
-
-//             //set user to header
-//             req.user = verifyUser;
-  
-//             //guard user
-//             if (roles.length && !roles.includes(verifyUser.role )) {
-//                 throw new ApiError(StatusCodes.FORBIDDEN,"You don't have permission to access this api");
-//             }
-//             next();
-//         }
-//     } catch (error) {
-//         next(error);
-//     }
-// }
 const auth = (...roles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
   try {
     const tokenWithBearer = req.headers.authorization;
@@ -52,7 +23,6 @@ const auth = (...roles: string[]) => async (req: Request, res: Response, next: N
 
       req.user = verifyUser;
 
-      // ✅ Check both role and currentRole for flexibility
       const userRole = verifyUser.role || verifyUser.currentRole;
 
       if (roles.length && !roles.includes(userRole)) {
